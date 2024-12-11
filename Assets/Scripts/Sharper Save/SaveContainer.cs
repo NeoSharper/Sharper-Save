f66176f28eb541a7ef4f42ecf1546d43e73ebc23
Guilherme Sena <guilherme.senadeaguiar626@gmail.com>
2024-12-11 19:21:28 -0300

    Initial commit

---
+++ Assets/Scripts/Sharper Save/SaveContainer.cs
@@ -0,0 +1,14 @@
+﻿using SharperSave.DataClasses;
+using UnityEngine;
+
+namespace SharperSave
+{
+    /// <summary>
+    /// The save container that stores the SaveData class.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Save Container", menuName = "Sharper Save/Save Container")]
+    public class SaveContainer : ScriptableObject
+    {
+        public SaveData saveData;
+    }
+}
